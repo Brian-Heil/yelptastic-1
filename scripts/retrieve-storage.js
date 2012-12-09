012dc5760d208325c66f20aa06eb09783d081784
The Feiner Things <the.feiner.things.fall.2012@gmail.com>
2012-12-09 18:17:50 -0500

    fixed for loops

--- scripts/retrieve-storage.js
+++ scripts/retrieve-storage.js
@@ -1,32 +1,36 @@
 var searchFavorites = function(query){
     var feedback = [];
     var resultList = localStorage.getItem("results");
-    for (var x in resultList.businesses){
+    _.each(resultList.businesses, function(x){
         if(x.name.search(query) > -1 || x.tag.search(query) > -1 || x.notes.search(query) > -1){
             feedback.push(x);
         }
-    }
+    });
     return feedback;
 };
 var getFavoriteCategories = function(){
     var feedback = [];
     var resultList = localStorage.getItem("results");
-    for (var x in resultList.businesses){
-            feedback.push(x.categories); //returns the list of lists (of categories); from here Varun will try to find the broad-level category that this category belongs to 
+    //returns the list of lists (of categories); from here Varun will try to find the broad-level category that this category belongs to 
+    _.each(resultList.businesses, function(x){
+       feedback.push(x.categories); 
     }
+ );
     return feedback;    
 };
 var getFavoritesWithinCategory = function(query){
     var feedback = [];
     var resultList = localStorage.getItem("results");
-    for (var x in resultList.businesses){
+    
+   _.each(resultList.businesses, function(x){
         if(x.categories == query){
             feedback.push(x);
         }
-    }
+    });
     return feedback; 
 };
-var addBusinessToFavorite = function(business){
+
+function addBusinessToFavorite (business){
     var resultList = [];
     if(typeof localStorage.getItem("results") != 'undefined'){
         resultList = localStorage.getItem("results");
@@ -34,9 +38,8 @@ var addBusinessToFavorite = function(business){
      resultList.push(business);
      localStorage.setItem("results", resultList); 
 };
-
-var deleteBusinessFromStorage = function(business){
+function deleteBusinessFromStorage(business){
     var resultList = localStorage.getItem("results");
-    resultList.delete(business);
+    resultList.remove(business);
     localStorage.setItem("results", resultList);
 };
