c7cee3528819acbc14307bd7c9352ea3bf209139
The Feiner Things <the.feiner.things.fall.2012@gmail.com>
2012-12-09 16:33:26 -0500

    didnt add jenees extra lines

--- scripts/retrieve-storage.js
+++ scripts/retrieve-storage.js
@@ -1,10 +1,41 @@
-var getFavoritesCatergories = function(query){
+var searchFavorites = function(query){
+    //search crap.name, tags, category, crap.category
     var feedback = [];
     var resultList = localStorage.getItem("results");
     for (var x in resultList.businesses){
-        if(x.name.search("query") > -1 || x.tag.search("query") > -1 || x.notes.search("query") > -1){
+        if(x.name.search(query) > -1 || x.tag.search(query) > -1 || x.notes.search(query) > -1){
             feedback.push(x);
         }
     }
     return feedback;
+}
+var getFavoriteCategories = function(){
+    var feedback = [];
+    var resultList = localStorage.getItem("results");
+    for (var x in resultList.businesses){
+            feedback.push(x.categories); //returns the list of lists (of categories); from here Daniel will try to find the broad-level category that this category belongs to 
+    }
+    return feedback;    
+}
+var getFavoritesWithinCategory = function(query){
+    var feedback = [];
+    var resultList = localStorage.getItem("results");
+    for (var x in resultList.businesses){
+        if(x.categories.search("query") > -1){
+            feedback.push(x);
+        }
+    }
+    return feedback; 
+}
+var addBusinessToFavorite = function(business){
+    var resultList = localStorage.getItem("results");
+    var newList = resultList;
+    newList.push(business);
+    localStorage.setItem(newList, resultList);   
+}
+var deleteBusinessFromStorage = function(business){
+    var resultList = localStorage.getItem("results");
+    var newList = resultList;
+    newList.delete(business);
+    localStorage.setItem(newList, resultList);
 }
