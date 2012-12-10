0a9eadbc20c18437413c91b3135144889e4852bc
The Feiner Things <the.feiner.things.fall.2012@gmail.com>
2012-12-09 23:24:17 -0500

    fixed bug with parsing

--- scripts/query.js
+++ scripts/query.js
@@ -79,7 +79,9 @@ var queryYelp = function(query, search_type, callback, error) {
 
 /* Utility function to parse JSON and return JS object */
 var parseData = function(json){
-  return JSON && JSON.parse(json) || $.parseJSON(json);
+
+  //return JSON && JSON.parse(json) || jQuery.parseJSON(json);
+  return jQuery.parseJSON(json);
 };
 
 /* Frontend API method to search Yelp with provided query and location. */
