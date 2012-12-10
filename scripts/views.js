cc8893692048ac7a1e9a45a1934a486952b7a83f
Orlando Pineda <omp2114@gmail.com>
2012-12-09 22:12:35 -0500

    breadcrumbs and whatnot working

--- scripts/views.js
+++ scripts/views.js
@@ -1,7 +1,51 @@
+var browseFavorites = function() {
+  var current = $('.active1').text();
+  //alert("LOL" + current);
+  $('.active1').remove();
+ // $('body').triggerHandler('Home');
+  $('.breadcrumb').append('<li onclick="$(\'body\').triggerHandler('+ '\''+ current + '\''+')"><a href="#' + current +'">' + current + '</a> <span class="divider">/</span><li>');
+  $('.breadcrumb').append('<li class="active">' + 'Lookup' +'</li> ');
+  history.pushState({}, 'Yelptastic- Browse Favorite', '#bookmarksearch');
+  $('.columnCenter').empty();
+   var data = new Array({
+     title: 'Yelp',
+     image_url: 'http://s3-media2.ak.yelpcdn.com/bphoto/7DIHu8a0AHhw-BffrDIxPA/ms.jpg',
+     snippet_text: 'Sometimes we ask questions without reading an email thoroughly as many of us did for the last event.  In honor of Yelp, the many questions they kindly...',
+    // tags: 'sucky',
+     rating_image_url_small:'http://media1.ak.yelpcdn.com/static/201012161694360749/img/ico/stars/stars_3.png',
+   },{title: 'cool',
+     image_url: 'http://placehold.it/300x200',
+     snippet_text: 'this sucks',
+    // tags: 'sucky',
+     rating_image_url_small: 'http://media3.ak.yelpcdn.com/static/201012161053250406/img/ico/stars/stars_large_3.png',
+   },{title: 'cool',
+     image_url: 'http://placehold.it/300x200',
+     snippet_text: 'this sucks',
+    // tags: 'sucky',
+     rating_image_url_small: 'http://media3.ak.yelpcdn.com/static/201012161053250406/img/ico/stars/stars_large_3.png',
+   },{title: 'cool',
+     image_url: 'http://placehold.it/300x200',
+     snippet_text: 'this sucks',
+    // tags: 'sucky',
+     rating_image_url_small: 'http://media3.ak.yelpcdn.com/static/201012161053250406/img/ico/stars/stars_large_3.png',
+       });
+    browseBookmarks(data, 0);
+
+}
+
+
+/*
+ * Displays the view for searching yelp.
+ */
 var addFavoritesView = function() {
-  alert('printing');
-  alert('length' + history.length);
-  history.pushState({}, 'Yelptastic- Yelp Search', document.URL + '/yelpsearch');
+  //$('.breadcrumbs').removeClass('.active');
+  var current = $('.active1').text();
+  //alert("LOL" + current);
+  $('.active1').remove();
+ // $('body').triggerHandler('Home');
+  $('.breadcrumb').append('<li onclick="$(\'body\').triggerHandler('+ '\''+ current + '\''+')"><a href="#' + current +'">' + current + '</a> <span class="divider">/</span><li>');
+  $('.breadcrumb').append('<li class="active">' + 'Search' +'</li> ');
+  history.pushState({}, 'Yelptastic- Yelp Search', '#yelpsearch');
   $('.columnCenter').empty();
    var data = new Array({
      title: 'Yelp',
@@ -63,6 +107,73 @@ var addQuery = function (results, opt_int) {
 
   }
   var source = $('#results').html();
+  var template = Handlebars.compile(source);
+  var string = template(data);
+        $see_prev = $('<input type="button" value="Previous 20" name="nextButton">');
+               $see_all = $('<input type="button" value="Next 20" name="prevButton">');
+
+
+
+  $thumbnailswrapper.append(string);
+  if (data.length >= 20) {
+    if (opt_int >= 20) {
+      alert('Running >');
+      $see_prev = $('<input type="button" value="Previous 20" name="nextButton">');
+      $see_prev.click(function() {
+        addQuery(results, opt_int-1);
+       });
+      $('.thumbnails').append($see_prev);
+      }
+    if (opt_int + 20 <= data.length) {
+      alert('Running <')
+       $see_all = $('<input type="button" value="Next 20" name="prevButton">');
+       $see_all.click(function() {
+          addQuery(results, opt_int + 20);
+       });
+      $('.thumbnails').append($see_all);
+    }
+  }  
+  $buttonRow = $('<div class="buttonRow"></div>');
+  $buttonRow.append($see_prev);
+  $buttonRow.append($see_all);
+  
+  $thumbnailswrapper.append($buttonRow);
+  $('.columnCenter').append($thumbnailswrapper);
+}
+
+/*
+ * Takes in the results and appends them to the thumbnails grids
+ */
+var browseBookmarks = function (results, opt_int) {
+  $('.columnCenter').empty();
+  $thumbnailswrapper = $('<ul class="thumbnails"></ul>');  
+  var data = [];
+  if (results.length >=20) {
+    for (var i = opt_int; i < opt_int + 20; i ++) {
+      $button = $('<button>Favorite</button>');
+      data.push({
+        title:results[i].title,
+        image:results[i].image_url,
+        quote:results[i].snippet_text,
+        ratingImage:results[i].rating_img_url_small,
+        tags:'',
+      });
+    }  
+  } else {
+    for (var i = opt_int; i < results.length; i ++) {
+      $button = $('<button>Favorite</button>');
+
+      data.push({
+        title:results[i].title,
+        image:results[i].image_url,
+        quote:results[i].snippet_text,
+        ratingImage:results[i].rating_image_url_small,
+        tags:'',
+      });
+    }  
+
+  }
+  var source = $('#browse').html();
   var template = Handlebars.compile(source);
   var string = template(data);
         $see_prev = $('<input type="button" value="Previous 20" name="nextButton">');
