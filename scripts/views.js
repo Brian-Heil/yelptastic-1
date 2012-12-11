96e814fda00b690cf06dbadf9b874293287d98cb
Varun Ravishankar <vrdabomb@gmail.com>
2012-12-11 14:08:32 -0500

    Add message when no bookmarks can be displayed.

--- scripts/views.js
+++ scripts/views.js
@@ -158,7 +158,6 @@ var addQuery = function (results, opt_int, opt_total, opt_term, opt_location, op
         $("#columnCenter").text("Sorry, there are no businesses to display.");
     }
 
-
     if (opt_total > 20) {
         $buttonRow = $('<div class="buttonRow"></div>');
 
@@ -244,7 +243,14 @@ var browseBookmarks = function (results, opt_int) {
     var template = Handlebars.compile(source);
     var string = template(data);
     spinner.stop();
-    $thumbnailswrapper.append(string);
+
+    if(results.length > 0){
+        $thumbnailswrapper.append(string);
+    }
+    else{
+        $("#columnCenter").text("Sorry, there are no favorites to display.");
+    }
+
     if (data.length >= 20) {
         if (opt_int >= 20) {
             $see_prev = $('<button class="btn" value="Previous" name="nextButton">Previous 20</button>');
