2dd0f26fadf9b75aa3828b4d8b142636977da20f
Varun Ravishankar <vrdabomb@gmail.com>
2012-12-11 13:21:42 -0500

    Fix button labels.

--- scripts/views.js
+++ scripts/views.js
@@ -155,14 +155,14 @@ var addQuery = function (results, opt_int, opt_total, opt_term, opt_location, op
         $buttonRow = $('<div class="buttonRow"></div>');
 
         if (opt_int >= 20) {
-            $see_prev = $('<button class="btn" value="Previous 20" name="nextButton">');
+            $see_prev = $('<button class="btn" value="Previous 20" name="prevButton">Previous 20</button>');
             $see_prev.click(function() {
                 searchYelp(opt_term, opt_location, opt_cat, opt_int-20);
             });
             $buttonRow.append($see_prev);
         }
         if (opt_int + 20 <= opt_total) {
-            $see_all = $('<button class="btn" value="Next 20" name="prevButton">');
+            $see_all = $('<button class="btn" value="Next 20" name="nextButton">Next 20</button>');
             $see_all.click(function() {
                 searchYelp(opt_term, opt_location, opt_cat, opt_int + 20);
             });
@@ -239,14 +239,14 @@ var browseBookmarks = function (results, opt_int) {
     $thumbnailswrapper.append(string);
     if (data.length >= 20) {
         if (opt_int >= 20) {
-            $see_prev = $('<button class="btn" value="Previous" name="nextButton">Previous</button>');
+            $see_prev = $('<button class="btn" value="Previous" name="nextButton">Previous 20</button>');
             $see_prev.click(function() {
                 addQuery(results, opt_int-1);
             });
             $buttonRow.append($see_prev);
         }
         if (opt_int + 20 <= data.length) {
-            $see_all = $('<button class="btn" value="Next" name="prevButton">Next</button>');
+            $see_all = $('<button class="btn" value="Next" name="prevButton">Next 20</button>');
             $see_all.click(function() {
                 addQuery(results, opt_int + 20);
             });
